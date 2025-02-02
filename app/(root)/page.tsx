901d36f07aa19052e9b1616259e98870d7d86385
Bhupesh Bora <bhupeshbora01@gmail.com>
2025-02-02 20:03:38 +0530

    initial

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+//  home page
+
+//  import Image from "next/image";
+import {Button} from "@/components/ui/button";
+
+const Home=() => (
+   <>
+    <Button>click mee</Button>
+   </>
+  );
+export default Home;
