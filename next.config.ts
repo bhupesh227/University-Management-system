5f8647732d1cf1c924fcf9482899a022ce53b932
Bhupesh Bora <bhupeshbora01@gmail.com>
2025-02-04 12:16:55 +0530

    optimizing the images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {                     
+    remotePatterns:[                          // remotePatterns is an array of objects that define the remote domains from which images are allowed to be optimized.
+      {
+        protocol: 'https',
+        hostname: "placehold.co",
+      },
+      {
+        protocol: 'https',
+        hostname: "m.media-amazon.com",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
