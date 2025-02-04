8dbc9173a1ae6131889eed95522ceadb2fc1fe56
Bhupesh Bora <bhupeshbora01@gmail.com>
2025-02-04 12:09:52 +0530

    added a bookoverview and booklist tag for homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,13 +3,20 @@
 //  import Image from "next/image";
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
 // import {Button} from "@/components/ui/button";
 
 const Home=() => (
    <>
-    <BookOverview/>
+    <BookOverview {...sampleBooks[0]}/>
 
-    <BookList/>
+    <BookList
+        //  books={sampleBooks}
+        title="Latest books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+
+    />
    </>
   );
 export default Home;
