9f39c2c959ed572fe960f745d6fea1edcb075fcd
Bhupesh Bora <bhupeshbora01@gmail.com>
2025-02-04 12:13:05 +0530

    conatin details represented about a book and passing a book cover prop through it

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,13 +1,68 @@
 import React from 'react'
+import Image from 'next/image'
+import { Button } from './ui/button';
+import BookCover from './BookCover';
 
-const BookOverview = () => {
-  return <section className='book-overview'>
-    <div className='flex flex-1 flex-col gap-5'>
-      <h1>a long book tytle</h1>
+// using Book as an interface i.e generic types
+const BookOverview = ({
+  title, 
+  author, 
+  genre, 
+  rating, 
+  total_Copies,
+  available_Copies,
+  description, 
+  coverColor, 
+  coverUrl, 
+}: Book) => {
+  return( 
+    <section className='book-overview'>
+      <div className='flex flex-1 flex-col gap-5'>
+        <h1>{title}</h1>
 
-    </div>
+        <div className='book-info'>
+            <p>By <span className='font-semibold text-light-200'>{author}</span></p>
+            <p>Category{" "} <span className='font-semibold text-light-200'>{genre}</span></p>
+            <div className='flex flex-row gap-1'>
+                <Image src="/icons/star.svg" alt="star" width={22} height={22} />
+                <p>{rating}</p>
+            </div>
+        </div>
 
-  </section>
+        <div className='book-copies'>
+            <p>Total Books <span>{total_Copies}</span></p>
+            <p>Available Books <span>{available_Copies}</span></p>
+        </div>
+
+        <p className='book-description'>{description}</p>
+
+        <Button className="book-overview_btn">
+          <Image src="/icons/book.svg" alt='book' width={20} height={20}></Image>
+          <p className='font-bebas-neue text-xl text-dark-100'>Borrow</p>
+        </Button>
+      </div>
+
+      <div className='relative flex flex-1 justify-center'>
+          <div className='relative'>
+            <BookCover
+              variant='wide'                                  // pass the variant prop
+              className='z-10'                                // appear above the image
+              coverColor={coverColor}                             // pass the coverColor prop
+              coverImage={coverUrl}                             // pass the coverImage prop
+            />
+
+            <div className='absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden'>
+              <BookCover
+                variant='wide'                                  // pass the variant prop
+                coverColor={coverColor}                             // pass the coverColor prop
+                coverImage={coverUrl}                             // pass the coverImage prop
+              />
+
+            </div>
+          </div>
+      </div>
+    </section>
+  );
  
 }
 
